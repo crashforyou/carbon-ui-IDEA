4bd60f673009b89df8be2f7709f9e35fd7b83136
alomsten <1194972692@qq.com>
2024-01-06 15:51:50 +0800

    1.修改了资金管理中的有关样式

--- src/views/bottomViews/CapitalServiceView/CapitalQuery.vue
+++ src/views/bottomViews/CapitalServiceView/CapitalQuery.vue
@@ -1,19 +1,20 @@
 <template>
   <a-tabs v-model:activeKey="activeKey">
-    <a-tab-pane key="1" tab="资金账号">
+    <a-tab-pane key="1" tab="资金账号" @click="showCapitalAccountSelect">
       <CapitalAccount/>
     </a-tab-pane>
-    <a-tab-pane key="2" tab="出入金流水" force-render>
+    <a-tab-pane key="2" tab="出入金流水" @click="showDepositAndWithdrawalRecordSelect" force-render>
       <CapitalFlow/>
     </a-tab-pane>
-    <a-tab-pane key="3" tab="资金交易流水">
+    <a-tab-pane key="3" @click="showCapitalTradeRecordSelect" tab="资金交易流水">
       <TransactionFlow/>
     </a-tab-pane>
-    <a-tab-pane key="4" tab="出入金申请流水">
+    <a-tab-pane key="4" @click="showDepositAndWithdrawalRequestRecordSelect" tab="出入金申请流水">
       <CapitalRequestFlow/>
     </a-tab-pane>
   </a-tabs>
 </template>
+
 
 <script  setup>
 import {ref} from "vue";
@@ -24,5 +25,45 @@ import CapitalRequestFlow from "@/views/bottomViews/CapitalServiceView/component
 import TransactionFlow from "@/views/bottomViews/CapitalServiceView/components/TransactionFlow.vue";
 
 const activeKey = ref("1");
+
+const showCapitalAccountSelect = () => {
+  // 询价查询并更新queryResult
+  activeKey.value = "1";
+};
+
+const showDepositAndWithdrawalRecordSelect = () => {
+  // 询价查询并更新queryResult
+  activeKey.value = "2";
+};
+
+const showCapitalTradeRecordSelect = () => {
+  // 询价查询并更新queryResult
+  activeKey.value = "3";
+};
+
+const showDepositAndWithdrawalRequestRecordSelect = () => {
+  // 询价查询并更新queryResult
+  activeKey.value = "4";
+};
 </script>
 
+<style scoped>
+button {
+  margin: 5px;
+  padding: 8px 13px;
+  font-size: 14px;
+  border: none;
+  border-radius: 5px;
+  background-color: #d4cfcc;
+}
+button:hover,button.active{
+  color: white;
+  background-color: #17294f;
+}
+.selectButton{
+  background: #eceff6;
+  border-top: 2px solid #a8b7d3;
+  border-bottom: 2px solid #a8b7d3;
+}
+
+</style>
