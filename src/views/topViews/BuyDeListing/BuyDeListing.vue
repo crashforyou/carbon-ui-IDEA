53087d469325030a5382e57502fc63c008b35a2d
crashforyou <doctorh-never@outlook.com>
2024-01-07 19:35:03 +0800

    买入摘牌、卖出摘牌、顶部按钮组

---
+++ src/views/topViews/BuyDeListing/BuyDeListing.vue
@@ -0,0 +1,68 @@
+<template>
+  <a-table :dataSource="dataSource" :columns="columns" />
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const dataSource = ref([
+  {
+    key: '1',
+    listingTime: '2022-01-01',
+    orderNumber: '123456',
+    itemCode: 'ABC',
+    itemName: 'Item 1',
+    customerNumber: 'C001',
+    listingQuantity: '100',
+    listingPrice: '10.00',
+  },
+]);
+
+const columns = ref([
+  {
+    title: '挂牌时间',
+    dataIndex: 'listingTime',
+    key: 'listingTime',
+  },
+  {
+    title: '委托编号',
+    dataIndex: 'orderNumber',
+    key: 'orderNumber',
+  },
+  {
+    title: '标的物代码',
+    dataIndex: 'itemCode',
+    key: 'itemCode',
+  },
+  {
+    title: '标的物名称',
+    dataIndex: 'itemName',
+    key: 'itemName',
+  },
+  {
+    title: '客户号',
+    dataIndex: 'customerNumber',
+    key: 'customerNumber',
+  },
+  {
+    title: '挂牌数量',
+    dataIndex: 'listingQuantity',
+    key: 'listingQuantity',
+  },
+  {
+    title: '挂牌价格',
+    dataIndex: 'listingPrice',
+    key: 'listingPrice',
+  },
+  {
+    title: '操作',
+    key: 'action',
+    fixed: 'right',
+    render: (text, record) => (
+        <a-space size="middle">
+          <a>摘牌</a>
+        </a-space>
+    ),
+  },
+]);
+</script>
---
+++ src/views/topViews/topView.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <div class="button-group">
+      <button @click="changeContent('BuyDeListing')">
+        <a-icon type="buy"/>
+        <span>买入摘牌</span>
+      </button>
+      <button @click="changeContent('SellDeListing')">
+        <a-icon type="sell"/>
+        <span>卖出摘牌</span>
+      </button>
+      <button @click="changeContent('Market')">
+        <a-icon type="market"/>
+        <span>市场</span>
+      </button>
+      <button @click="changeContent('TimeDivision')">
+        <a-icon type="time-sharing"/>
+        <span>分时</span>
+      </button>
+      <button @click="changeContent('KLine')">
+        <a-icon type="k-line"/>
+        <span>K线</span>
+      </button>
+      <button @click="changeContent('DealDivision')">
+        <a-icon type="part-by-part"/>
+        <span>分笔</span>
+      </button>
+    </div>
+    <div class="display-area">
+      <component :is="contentComponent"/>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+import Market from "@/views/topViews/Market/Market.vue";
+import KLine from "@/views/topViews/KLine/KLine.vue";
+import {
+  UserOutlined,
+  AreaChartOutlined,
+  InteractionOutlined,
+  TransactionOutlined,
+  CreditCardOutlined,
+  PoundOutlined,
+  ProfileOutlined,
+  ScheduleOutlined,
+  SwitcherOutlined,
+} from "@ant-design/icons-vue";
+
+const contentComponent = ref('');
+const changeContent = (newContent) => {
+  contentComponent.value = newContent;
+};
+
+</script>
+
+<style scoped>
+.button-group > button {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-right: 1em;
+}
+
+.display-area {
+  margin-top: 2em;
+  border: 1px solid #ccc;
+  padding: 1em;
+}
+</style>
